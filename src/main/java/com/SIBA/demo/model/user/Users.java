b2c7a660f3af12a8357663a9e1697441826ed78f
RAQEEB212 <saneelmemon45@gmail.com>
2023-05-04 17:08:47 +0500

    Students named table in spring boot

--- src/main/java/com/SIBA/demo/model/user/Users.java
+++ src/main/java/com/SIBA/demo/model/user/Users.java
@@ -1,4 +1,50 @@
 package com.SIBA.demo.model.user;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Students")
+
 
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int rollno;
+    private String name;
+    @javax.persistence.Id
+    private Long id;
+
+    public Users(int rollno,String name) {
+        this.rollno = rollno;
+        this.name = name;
+
+
+    }
+
+    public int getrollno() {
+        return rollno;
+    }
+
+    public String getname() {
+        return name;
+    }
+    public void setRollno(int rollno){
+        this.rollno = rollno;
+
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
 }
+
+
+
+
+
+
+
